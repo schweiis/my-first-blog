2ed46d1a10022c3cabe87bd8a628107e8c48ddc5
Isabell Schweizer <IsabellSchweizer@gmx.de>
2019-05-22 08:36:38 +0200

    bla

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+
+def post_list(request):
+    name = "Isabell"
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts, "name": name})
